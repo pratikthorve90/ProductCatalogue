ba5d013bb518842578137af621650292eef57757
Pratik Thorve <pratik.thorve@priceline.com>
2026-03-12 15:31:31 +0530

    first commit

---
+++ src/test/java/com/pratik/productcatalogue/ProductcatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratik.productcatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
